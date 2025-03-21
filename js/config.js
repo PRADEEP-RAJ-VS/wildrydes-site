1ac6c4b69220a389c1955d79c0ae178d9d69e092
Pradeep Raj <146763897+PRADEEP-RAJ-VS@users.noreply.github.com>
2025-03-21 20:38:52 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Ehj8gOs1R', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4g2spnlieefhk94h3kpljvq40l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_Ehj8gOs1R',
+        userPoolClientId: '4g2spnlieefhk94h3kpljvq40l',
+        region: 'us-east-1',
+        userPoolClientSecret: '1a39chbvq80vdga3cl3ahk677q10eiirk8ca3ak5oi28i0a6admu' // Replace with your actual client secret
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: ''
     }
 };
