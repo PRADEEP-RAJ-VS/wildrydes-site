f4f032cb5d0cd1c3db0c93b9c5e00a5a7df1b660
Pradeep Raj <146763897+PRADEEP-RAJ-VS@users.noreply.github.com>
2025-03-22 00:49:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,13 @@
+// js/config.js
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_Ehj8gOs1R',
         userPoolClientId: '4g2spnlieefhk94h3kpljvq40l',
         region: 'us-east-1',
-        userPoolClientSecret: '1a39chbvq80vdga3cl3ahk677q10eiirk8ca3ak5oi28i0a6admu' 
+        // REMOVE THIS LINE FOR PRODUCTION. THIS IS ONLY FOR LOCAL TESTING.
+        // userPoolClientSecret: 'YOUR_CLIENT_SECRET'
     },
     api: {
-        invokeUrl: ''
+        invokeUrl: 'YOUR_BACKEND_API_ENDPOINT' // Replace with your backend API endpoint
     }
 };
