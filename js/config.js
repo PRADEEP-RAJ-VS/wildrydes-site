13bbbf210457fd951e6966699aea31f188a47e26
Pradeep Raj <146763897+PRADEEP-RAJ-VS@users.noreply.github.com>
2025-03-21 20:22:04 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Ehj8gOs1R', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4g2spnlieefhk94h3kpljvq40l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
